9ce307c92d72a94a9115e5c98fc6a453704c628a
Julio Hernandez <julioandres72@gmail.com>
2020-11-18 19:54:58 -0500

    done

--- lib/author.rb
+++ lib/author.rb
@@ -2,7 +2,6 @@ require 'pry'
 
 class Author
   attr_accessor :name
-
   @@all = []
 
   def initialize(name)
@@ -18,16 +17,81 @@ class Author
     post.author = self
   end
 
+  def posts
+    Post.all.select{|post| post.author == self}
+  end
+
   def add_post_by_title(title)
     post = Post.new(title)
     post.author = self
-  end
 
-  def posts
-    Post.all.select {|post| post.author == self}
   end
 
   def self.post_count
     Post.all.count
   end
+
+
+
+
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Author
+#   attr_accessor :name
+
+#   @@all = []
+
+#   def initialize(name)
+#     @name = name
+#     @@all << self
+#   end
+
+#   def self.all
+#     @@all
+#   end
+
+#   def add_post(post)
+#     post.author = self
+#   end
+
+#   def add_post_by_title(title)
+#     post = Post.new(title)
+#     post.author = self
+#   end
+
+#   def posts
+#     Post.all.select {|post| post.author == self}
+#   end
+
+#   def self.post_count
+#     Post.all.count
+#   end
+# end
--- lib/post.rb
+++ lib/post.rb
@@ -1,18 +1,69 @@
+
 class Post
-    attr_accessor :title, :author
-  
-    @@all = []
-  
-    def initialize(title)
-      @title = title
-      @@all << self
-    end
-  
-    def self.all
-      @@all
-    end
-  
-    def author_name
-      author.name if author
-    end
+  attr_accessor :title, :author
+  @@all = []
+
+  def initialize(title)
+    @title = title
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def author_name
+    author.name if author
+  end
+
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Post
+#     attr_accessor :title, :author
+  
+#     @@all = []
+  
+#     def initialize(title)
+#       @title = title
+#       @@all << self
+#     end
+  
+#     def self.all
+#       @@all
+#     end
+  
+#     def author_name
+#       author.name if author
+#     end
+#   end
